bb9e6a23cd0900ed728b594553b764a34ef54ccb
yuzo <yuzokamoto@gmail.com>
2022-11-04 15:04:10 -0300

    bananinha

---
+++ propriedade-metodo.js
@@ -0,0 +1,18 @@
+// Propriedade de um objeto
+// pode ou não ser uma função
+// normalmente significa que não é uma função
+
+
+// Método de um objeto
+// é sempre uma função
+
+
+const obj = {
+    prop: null,
+    method: () => {}
+}
+
+
+// TLDR:
+// método é sempre função
+// outros sinônimos: rotina, ação, comportamento
