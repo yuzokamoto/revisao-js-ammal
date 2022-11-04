966bf81ed6a301787a7adce1d708b4b769fa063c
yuzo <yuzokamoto@gmail.com>
2022-11-04 16:17:09 -0300

    bananinha

--- espalhamento-array.js
+++ espalhamento-array.js
@@ -1,3 +1,35 @@
+// O operador de espalhamento é utilizado para copiar um novo objeto e array
+
+const users = [
+    {
+        id: 1,
+        name: "Fulano"
+    },
+    {
+        id: 2,
+        name: "Ciclana"
+    },
+    {
+        id: 3,
+        name: "Beltrana"
+    }
+]
+
+
+const copiaUsers = [
+    {
+        id: 0,
+        name: "Bananinha"
+    },
+    ...users,
+    {
+        id: 4,
+        name: "Astrodev"
+    }
+]
+
+console.log(users)
+console.log(copiaUsers)
 
 
 
@@ -5,13 +37,10 @@
 
 
 
-
-
-
-
-
-
-
+function somaNums(a, b) {
+    return a + b
+}
 
 const arg = [5, 10]
-console.log(copiaObj.method1(...arg)) // significa que os argumentos de uma função nada mais são que itens de um array
+console.log(somaNums(...arg))
+// significa que os argumentos de uma função nada mais são que itens de um array
